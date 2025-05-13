dbe09ded39959ebcf7e3559bf5ff039692d21194
Joseph Vranicar <josephvranicar1@gmail.com>
2025-05-13 18:39:16 -0400

    c

--- autocolorscheme.sh
+++ autocolorscheme.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-colorscheme=$(python3 ./gethour.py)
+# colorscheme=$(python3 autoset-plasma-colorscheme/gethour.py)
+colorscheme=$(node autoset-plasma-colorscheme/gethour.js)
 plasma-apply-colorscheme $colorscheme
---
+++ gethour.js
@@ -0,0 +1,1 @@
+console.log(new Date().getHours() > 6 && new Date().getHours() < 18 ? "BreezeLight" : "BreezeDark");
