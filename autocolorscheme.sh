7f9ec176467ca45b31931e642aad9c344757e145
Joseph Vranicar <josephvranicar1@gmail.com>
2025-05-13 20:53:41 -0400

    c

--- autocolorscheme.sh
+++ autocolorscheme.sh
@@ -6,11 +6,19 @@ logged_color=""
 if [ -f "current_colors.txt" ]; then
   logged_color=$(head -n 1 current_colors.txt)
 
+else
+  touch current_colors.txt
 
+fi
 
 if [ "$logged_color" != "$colorscheme" ]; then
   plasma-apply-colorscheme $colorscheme
   echo $colorscheme > current_colors.txt
+
+elif [ -f "current_colors.txt" == false]; then
+  
+
 else
   echo "$colorscheme is already in use!"
+
 fi
