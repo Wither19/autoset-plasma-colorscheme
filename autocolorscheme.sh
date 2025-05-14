039a1de2c6d0f41bdc5ad0c854f6ab30e7afd074
Joseph Vranicar <josephvranicar1@gmail.com>
2025-05-13 20:47:38 -0400

    c

--- autocolorscheme.sh
+++ autocolorscheme.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-# colorscheme=$(python3 autoset-plasma-colorscheme/gethour.py)
-colorscheme=$(node autoset-plasma-colorscheme/gethour.js)
-plasma-apply-colorscheme $colorscheme
+colorscheme=$(node ./gethour.js)
+logged_color=""
+
+
+if [ -f "current_colors.txt" ]; then
+  logged_color=$(head -n 1 current_colors.txt)
+
+
+
+if [ "$logged_color" != "$colorscheme" ]; then
+  plasma-apply-colorscheme $colorscheme
+  echo $colorscheme > current_colors.txt
+else
+  echo "$colorscheme is already in use!"
+fi
