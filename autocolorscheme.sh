9099de3d6e9a379086e6d78e782a2d136e8f7a51
Joseph Vranicar <josephvranicar1@gmail.com>
2025-05-26 18:31:22 -0400

    c

--- autocolorscheme.sh
+++ autocolorscheme.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
-colorscheme=$(python3 gethour.py)
-plasma-apply-colorscheme "$colorscheme"
+colorscheme=$(python3 ~/scripts/autoset-plasma-colorscheme/gethour.py)
+
+light_colors="AritimLight"
+dark_colors="MateriaDark"
+
+if [ "$colorscheme" == "light" ]; then
+  plasma-apply-colorscheme "$light_colors"
+
+elif [ "$colorscheme" == "dark" ]; then
+  plasma-apply-colorscheme "$dark_colors"
+
+else
+  echo "Could not apply color scheme"
+
+fi
--- gethour.py
+++ gethour.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
   current_hour = datetime.now().hour
   # Light mode runs from 7 AM to 6 PM
   if (current_hour > 6 and current_hour < 18):
-    print("BreezeLight")
+    print("light")
   else:
-    print("BreezeDark")
+    print("dark")
