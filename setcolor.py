cf4f12ddc178e7592fb39ea3fec579dd56994fa4
Joseph Vranicar <josephvranicar1@gmail.com>
2025-05-26 10:09:27 -0400

    c

--- setcolor.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-if (len(sys.argv) > 1):
-  print("Breeze" + sys.argv[1].capitalize())
--- setcolor.sh
+++
@@ -1,9 +0,0 @@
-theme=$1
-
-result=$(python3 scripts/autoset-plasma-colorscheme/setcolor.py)
-
-echo "$result theme"
-
-if [ "$result" != "" ]; then
-  plasma-apply-colorscheme "$result"
-fi
